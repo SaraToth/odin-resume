9271f45ebfeda1b989ffd554085836c250cc4539
SaraToth <saratoth101@gmail.com>
2025-03-22 21:54:56 +0900

    Feat: Submit JobSum reverts back to JobList page
    Add functionality that when user submits their JobSum component page form, it will send them to a third conditional rendering on JobList that will be where the logic displaying all job info will be housed.

--- src/components/JobList.jsx
+++ src/components/JobList.jsx
@@ -6,13 +6,19 @@ export default function JobList() {
     const [currentSubPage, setCurrentSubPage] = useState("job");
 
     const handleSubmit = () =>{
-        setCurrentSubPage("jobSum")
+        if(currentSubPage === "job") {setCurrentSubPage("jobSum")}
+        if(currentSubPage === "jobSum") {setCurrentSubPage("jobList")}
     }
     
     return(
         <div>
             {currentSubPage === "job" && <Job onSubmit={handleSubmit} /> }
-            {currentSubPage === "jobSum" && <JobSum />}
+            {currentSubPage === "jobSum" && <JobSum onSubmit={handleSubmit}/>}
+            {currentSubPage === "jobList" 
+            && <div>
+                <h1>You submitted job and jubsum!</h1>
+            </div>
+}
         </div>
     )
 }
--- src/components/JobSum.jsx
+++ src/components/JobSum.jsx
@@ -1,5 +1,49 @@
-export default function JobSum() {
-    return(
-        <h1>Job Summary</h1>
-    )
+import { useState } from "react"
+
+export default function JobSum({onSubmit}) {
+    const [formStatus, setFormStatus] = useState({isEditing: true, isDisplaying: false});
+    const [formData, setFormData] = useState({
+        summary: "",
+    });
+
+    const handleSubmit = (e)=> {
+        e.preventDefault();
+
+        const newFormData = {
+            ...formData,
+            [e.target.name]: e.target.value,
+        }
+
+        setFormData(newFormData);
+        setFormStatus({isEditing: false, isDisplaying: true});
+        onSubmit()
+    }
+    
+    const handleChange = (e)=>{
+        const newFormData = {
+            ...formData,
+            [e.target.name]: e.target.value
+        }
+
+        setFormData(newFormData);
+    }
+
+    if(formStatus.isEditing && !formStatus.isDisplaying) {
+        return(
+            <div className="form-container">
+                <form onSubmit={handleSubmit}>
+                    <h1>Job Summary</h1>
+                    <textarea name="summary" id="summary" value={formData.summary} onChange={handleChange} required>Type your job description here</textarea>
+                    <button type="submit" className="submit-btn">Submit</button>
+                </form>
+            </div>
+
+        )
+    }
+
+    if(!formStatus.isEditing && formStatus.isDisplaying) {
+        return(
+            <h1>Submited</h1>
+        )
+    }
 }
