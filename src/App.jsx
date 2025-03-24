790ef5d5fb0e52fb3fa3eee0a584e07611f507b7
SaraToth <saratoth101@gmail.com>
2025-03-24 20:12:55 +0900

    Feat: Add Resume as a page option to App
    Update App changePage function to also switch to the Resume component page.

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import GenInfo from './pages/GenInfo'
 import Footer from './components/Footer'
 import JobList from './pages/JobList';
 import DegreeList from './pages/DegreeList';
+import Summary from './pages/Summary';
+import Resume from './pages/Resume';
 
 function App() {
 
@@ -13,13 +15,17 @@ function App() {
   const changePage = () =>{
     if(currentPage === "genInfo") setCurrentPage("jobList");
     if(currentPage === "jobList") setCurrentPage("degreeList");
+    if(currentPage === "degreeList") setCurrentPage("summary");
+    if(currentPage === "summary") setCurrentPage("resume");
   }
 return (
   <div className="app-container">
     <div className="main">
       {currentPage === "genInfo" && <GenInfo onSubmit={changePage} />}
       {currentPage === "jobList" && <JobList onClick={changePage} />}
-      {currentPage === "degreeList" && <DegreeList />}
+      {currentPage === "degreeList" && <DegreeList onClick={changePage} />}
+      {currentPage === "summary" && <Summary onSubmit={changePage}/>}
+      {currentPage === "resume" && <Resume />}
     </div>
     <Footer />
   </div>
