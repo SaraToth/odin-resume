8e1094f52fe3300d4b60b1ee3cbc92370bd14ece
SaraToth <saratoth101@gmail.com>
2025-03-24 19:37:46 +0900

    Feat: Add customHandleSubmit to Degree component
    customHandleSubmit function allows Degree to send formData back to its parent component DegreeList

---
+++ src/components/Degree.jsx
@@ -0,0 +1,37 @@
+import useForm from "../hooks/useForm";
+
+export default function Degree({onSubmit}) {
+    const {formData, handleChange, handleSubmit, formStatus} = useForm(
+        {
+            name: "",
+            school: "",
+            date: "",
+        },
+        onSubmit
+    );
+
+    const customHandleSubmit = (e) => {
+        e.preventDefault();
+
+        onSubmit(formData);
+    }
+
+    if(formStatus.isEditing && !formStatus.isDisplaying) {
+        return(
+            <div className="form-container">
+                <h1>Degree Information</h1>
+                <form onSubmit={customHandleSubmit} >
+                    <label htmlFor="name">Name</label>
+                    <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} required></input>
+
+                    <label htmlFor="school">School</label>
+                    <input type="text" id="school" name="school" value={formData.school} onChange={handleChange} required></input>
+
+                    <label htmlFor="date">Graduation Date</label>
+                    <input type="date" id="date" name="date" value={formData.date} onChange={handleChange} required></input>
+                    <button type="subumit" className="submit-btn">Submit</button>
+                </form>
+            </div>
+        )
+    }
+}
