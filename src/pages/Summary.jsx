75d66daddabac52cc553131052463c76924abb41
SaraToth <saratoth101@gmail.com>
2025-03-24 20:10:01 +0900

    Feat: Create career Summary component
    Create the Summary component which will prompt user to enter their career summary.

---
+++ src/pages/Summary.jsx
@@ -0,0 +1,42 @@
+import useForm from "../hooks/useForm";
+
+export default function Summary({onSubmit}) {
+    const {formData, handleChange, formStatus} = useForm(
+        {
+            summary: "",
+        },
+        onSubmit
+    );   
+
+    const customHandleSubmit = (e) => {
+        e.preventDefault();
+
+        onSubmit(formData);
+    } 
+
+    if(formStatus.isEditing && !formStatus.isDisplaying) {
+        return (
+            <div className="form-container">
+            <form onSubmit={customHandleSubmit}>
+                <h1>Career Summary</h1>
+                <textarea name="summary" id="summary" value={formData.summary} onChange={handleChange} required>Type your job description here</textarea>
+                <button type="submit" className="submit-btn">Submit</button>
+            </form>
+        </div>
+        )
+    }
+
+    if(!formStatus.isEditing && formStatus.isDisplaying) {
+        return (
+            <div className="display-container">
+                <div className="display-content">
+                    <h1>Career Summary</h1>
+                    <div className="display-contact">
+                        <div>{formData.summary}</div>
+                    </div>
+                </div>
+            </div>
+
+        )
+    }
+}
