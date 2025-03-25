1254eb7902df55c388f28dbbb7f477c1914bfa14
SaraToth <saratoth101@gmail.com>
2025-03-25 13:54:01 +0900

    Feat: Remove formStatus from Degree component
    The degree component form info will be stored as a state array on App.jsx so its not necessary have conditional rendering to display this info when it can be rendered directly from App.jsx. Degree component can just function to return a form.

--- src/components/Degree.jsx
+++ src/components/Degree.jsx
@@ -1,7 +1,7 @@
 import useForm from "../hooks/useForm";
 
 export default function Degree({onSubmit}) {
-    const {formData, handleChange, formStatus} = useForm(
+    const {formData, handleChange} = useForm(
         {
             name: "",
             school: "",
@@ -16,22 +16,20 @@ export default function Degree({onSubmit}) {
         onSubmit(formData);
     }
 
-    if(formStatus.isEditing && !formStatus.isDisplaying) {
-        return(
-            <div className="form-container">
-                <h1>Degree Information</h1>
-                <form onSubmit={customHandleSubmit} >
-                    <label htmlFor="name">Name</label>
-                    <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} required></input>
+    return(
+        <div className="form-container">
+            <h1>Degree Information</h1>
+            <form onSubmit={customHandleSubmit} >
+                <label htmlFor="name">Name</label>
+                <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} required></input>
 
-                    <label htmlFor="school">School</label>
-                    <input type="text" id="school" name="school" value={formData.school} onChange={handleChange} required></input>
+                <label htmlFor="school">School</label>
+                <input type="text" id="school" name="school" value={formData.school} onChange={handleChange} required></input>
 
-                    <label htmlFor="date">Graduation Date</label>
-                    <input type="date" id="date" name="date" value={formData.date} onChange={handleChange} required></input>
-                    <button type="subumit" className="submit-btn">Submit</button>
-                </form>
-            </div>
-        )
-    }
+                <label htmlFor="date">Graduation Date</label>
+                <input type="date" id="date" name="date" value={formData.date} onChange={handleChange} required></input>
+                <button type="subumit" className="submit-btn">Submit</button>
+            </form>
+        </div>
+    )
 }
