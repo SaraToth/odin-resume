1c05aa121c5549cb1eed2ad260f9c2afa78b5a78
SaraToth <saratoth101@gmail.com>
2025-03-25 17:08:23 +0900

    Feat: Remove formStatus from custom hook
    This functionality is no longer needed and was entirely phased out, so is not needed in the custom hook.

--- src/hooks/useForm.jsx
+++ src/hooks/useForm.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 
 export default function useForm(initialData, onSubmit) {
         const [formData, setFormData] = useState(initialData);
-        const [formStatus, setFormStatus] = useState({isEditing: true, isDisplaying: false})
     
         const handleChange = (e) => {
             const newFormData = {
@@ -23,12 +22,11 @@ export default function useForm(initialData, onSubmit) {
 
             if(onSubmit) {
                 setFormData(newFormData);
-                setFormStatus({isEditing: false, isDisplaying: true})
                 onSubmit();
             }
 
             setFormData(initialData);
         }
 
-        return {formData, setFormData, handleChange, handleSubmit, setFormStatus, formStatus};
+        return {formData, setFormData, handleChange, handleSubmit};
 }
