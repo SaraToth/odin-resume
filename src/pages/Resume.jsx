71f639ec9459852ff91c22683b1b0b7af8ac5cbf
SaraToth <saratoth101@gmail.com>
2025-03-25 17:36:39 +0900

    Feat: Render gen info and jobs in Resume component
    Begin to create rendering logic to render the final resume on Resume page.

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,12 +1,35 @@
 import Summary from "./Summary";
 import GenInfo from "./GenInfo";
+import JobItem from "../components/JobItem";
 
-export default function Resume({jobs, degrees, GenInfo, summary }) {
+export default function Resume({jobs, degrees, genInfo, summary }) {
 
+    const name = genInfo.first + " " + genInfo.last;
 
     return (
         <div>
+            <div>
+                <h1>{name}</h1>
+                <h2>{GenInfo.title}</h2>
+                <div className="display-contact">
+                    <div>{genInfo.phone}</div>
+                    <div>{genInfo.email}</div>
+                </div>
+            </div>
 
+            <div>
+                <p>summary</p>
+            </div>
+
+            <div>
+                <h3>Work Experience</h3>
+                {jobs.map((job) => <JobItem key={job.jobID} job={job} />)}
+            </div>
+
+            <div>
+                <h3>Education</h3>
+                <p>TBA</p>
+            </div>
         </div>
     )
 
