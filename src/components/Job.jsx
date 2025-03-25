bfdeb7b3d560605a2445214a74ccd87deeecd35a
SaraToth <saratoth101@gmail.com>
2025-03-25 13:52:05 +0900

    Feat: Remove formStatus from Job and JobSum
    Form status does not need to be stored as a state, since the job info and job summary are stored into an array state jobs on App.jsx these components are not needed to conditionally render job values, and can just simply be form only components.

--- src/components/Job.jsx
+++ src/components/Job.jsx
@@ -2,7 +2,7 @@ import useForm from "../hooks/useForm"
 
 export default function Job({onSubmit}) {
 
-    const {formData, handleChange, formStatus} = useForm(
+    const {formData, handleChange } = useForm(
         {
             title: "",
             company: "",
@@ -18,33 +18,28 @@ export default function Job({onSubmit}) {
         onSubmit(formData);
     }
 
-    if(formStatus.isEditing && !formStatus.isDisplaying) {
-        return (
-            <div className="form-container">
-                <h1>Job Info</h1>
-                <form onSubmit={customHandleSubmit}>
-                    <label htmlFor="title">Title:</label>
-                    <input type="text" id="title" name="title" value={formData.title} onChange={handleChange} required></input>
     
-                    <label htmlFor="company">Company:</label>
-                    <input type="text" id="company" name="company" value={formData.company} onChange={handleChange} required></input>
-    
-                    <label htmlFor="start">Start Date:</label>
-                    <input type="date" id="start" name="start" value={formData.start} onChange={handleChange} required></input>
-    
-                    <label htmlFor="end">End Date:</label>
-                    <input type="date" id="end" name="end" value={formData.end} onChange={handleChange} required></input>
-    
-                    <button type="submit" className="submit-btn">Submit</button>
-                </form>
-            </div>
-        )
-    }
+    return (
+        <div className="form-container">
+            <h1>Job Info</h1>
+            <form onSubmit={customHandleSubmit}>
+                <label htmlFor="title">Title:</label>
+                <input type="text" id="title" name="title" value={formData.title} onChange={handleChange} required></input>
 
-    if(!formStatus.isEditing && formStatus.isDisplaying) {
-        return(
-            <h1>Job info would be here</h1>
-        )
-    }
+                <label htmlFor="company">Company:</label>
+                <input type="text" id="company" name="company" value={formData.company} onChange={handleChange} required></input>
+
+                <label htmlFor="start">Start Date:</label>
+                <input type="date" id="start" name="start" value={formData.start} onChange={handleChange} required></input>
+
+                <label htmlFor="end">End Date:</label>
+                <input type="date" id="end" name="end" value={formData.end} onChange={handleChange} required></input>
+
+                <button type="submit" className="submit-btn">Submit</button>
+            </form>
+        </div>
+    )
+
+
 
 }
--- src/components/JobSum.jsx
+++ src/components/JobSum.jsx
@@ -2,7 +2,7 @@ import useForm from "../hooks/useForm";
 
 export default function JobSum({onSubmit}) {
 
-    const {formData, handleChange, formStatus} = useForm(
+    const {formData, handleChange} = useForm(
         {
             summary: "",
         },
@@ -15,22 +15,15 @@ export default function JobSum({onSubmit}) {
         onSubmit(formData);
     }
 
-    if(formStatus.isEditing && !formStatus.isDisplaying) {
-        return(
-            <div className="form-container">
-                <form onSubmit={customHandleSubmit}>
-                    <h1>Job Summary</h1>
-                    <textarea name="summary" id="summary" value={formData.summary} onChange={handleChange} required>Type your job description here</textarea>
-                    <button type="submit" className="submit-btn">Submit</button>
-                </form>
-            </div>
+    return(
+        <div className="form-container">
+            <form onSubmit={customHandleSubmit}>
+                <h1>Job Summary</h1>
+                <textarea name="summary" id="summary" value={formData.summary} onChange={handleChange} required>Type your job description here</textarea>
+                <button type="submit" className="submit-btn">Submit</button>
+            </form>
+        </div>
 
-        )
-    }
+    )
 
-    if(!formStatus.isEditing && formStatus.isDisplaying) {
-        return(
-            <h1>Job sum would be here</h1>
-        )
-    }
 }
