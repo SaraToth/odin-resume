dd0744e98147e861d2022eab21595f872930f6f2
SaraToth <saratoth101@gmail.com>
2025-03-25 17:05:55 +0900

    Feat: Create genInfo and sum states
    Remove all formStatus related code and instead create genInfo and summary states to pass as props to GenInfo and Summary components to collect data from those forms.

--- src/App.jsx
+++ src/App.jsx
@@ -14,12 +14,8 @@ function App() {
 
   const [jobs, setJobs] = useState([]); // Stores ALL submitted job data in an array
   const [degrees, setDegrees] = useState([]); // Stores ALL submitted degree data in an array
-
-  // Form Status state for each form page
-  const [formStatus, setFormStatus] = useState({
-    genInfo: { isEditing: true, isDisplaying: false},
-    summary: { isEditing: true, isDisplaying: false},
-  });
+  const [genInfo, setGenInfo] = useState([]);
+  const [summary, setSummary] = useState([]);
 
   const changePage = () =>{
     if(currentPage === "genInfo") setCurrentPage("jobList");
@@ -30,11 +26,11 @@ function App() {
 return (
   <div className="app-container">
     <div className="main">
-      {currentPage === "genInfo" && <GenInfo formStatus={formStatus.genInfo} onSubmit={changePage} />}
+      {currentPage === "genInfo" && <GenInfo setGenInfo={setGenInfo} onSubmit={changePage} />}
       {currentPage === "jobList" && <JobList jobs={jobs} setJobs={setJobs} onClick={changePage} />}
       {currentPage === "degreeList" && <DegreeList degrees={degrees} setDegrees={setDegrees} onClick={changePage} />}
-      {currentPage === "summary" && <Summary formStatus={formStatus.summary} onSubmit={changePage}/>}
-      {currentPage === "resume" && <Resume />}
+      {currentPage === "summary" && <Summary setSummary={setSummary} onSubmit={changePage}/>}
+      {currentPage === "resume" && <Resume jobs={jobs} degrees={degrees} genInfo={genInfo} summary={summary} />}
     </div>
     <Footer />
   </div>
