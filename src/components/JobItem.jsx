62d89e29c8fba3de1202bae06e6c6fc9fc4f49c4
SaraToth <saratoth101@gmail.com>
2025-03-25 17:42:08 +0900

    Feat: Job edit btn id matches jobID
    This allows direct targeting of that job object in jobs state when btn is clicked.

--- src/components/JobItem.jsx
+++ src/components/JobItem.jsx
@@ -5,7 +5,7 @@ export default function JobItem({job}) {
             <h2>{job.title} at {job.company}</h2> 
             <p>{job.start} - {job.end}</p> 
             <p>{job.summary}</p> 
-            <button type="submit" className="edit-btn">Edit</button>
+            <button id={job.jobID} type="submit" className="edit-btn">Edit</button>
         </div>
     );
 }
